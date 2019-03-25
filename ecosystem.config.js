f227b2ff26c43516fe724be58184d83a11cea437
Aditya Swami <aswami@hatcher.com>
2019-03-25 21:39:46 +0800

    change ip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-54-169-251-113.ap-southeast-1.compute.amazonaws.com',
+      host: 'ec2-18-136-207-103.ap-southeast-1.compute.amazonaws.com',
       key: '/Users/adityaswami/Desktop/cs5224project.pem',
       ref: 'origin/master',
       repo: 'git@github.com:adityaswami93/hyperlocus.git',
