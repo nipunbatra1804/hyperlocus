2744b87d31929dbe1f1fd823a183f9f9c14ad004
Aditya Swami <aswami@hatcher.com>
2019-03-26 20:19:23 +0800

    Login route

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.post('/login', (req, res) => {
       else res.sendStatus(401)
     })
 })
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000')
+app.listen(7555, () => {
+  console.log('Server running on http://localhost:7555')
 })
