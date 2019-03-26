3593f2f503c62969dfb9627ba38c8afbe389af36
Aditya Swami <aswami@hatcher.com>
2019-03-27 00:31:59 +0800

    on port 3000

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.post('/login', (req, res) => {
 }) 
 
 app.listen(3000, () => {
-  console.log('Server running on http://localhost:7555')
+  console.log('Server running on http://localhost:3000')
 })
