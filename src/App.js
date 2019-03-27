9bb20f8281955101bb0b42edf1de591cc0b106b1
Aditya Swami <aswami@hatcher.com>
2019-03-27 22:47:05 +0800

    Revert "Revert "add react components""
    This reverts commit d634366f67d4024665abc558b884829b3ed9040c.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import AppPage from "./components/AppPage/AppPage";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppPage />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MapGL/LocationPin.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const pinStyle = {
+  cursor: "pointer",
+  fill: "#d00",
+  stroke: "none"
+};
+const getClassName = type => {
+  switch (type) {
+    case "supermarket":
+      return "fas fa-cart-arrow-down";
+    case "clinic":
+      return "fas fa-clinic-medical";
+    case "hawkerCentre":
+      return "fas fa-utensils";
+    default:
+      return "far fa-compass";
+    //<i class="fab fa-500px"></i>
+  }
+};
+
+export default function LocationPin(props) {
+  const { size = 20, onClick, type } = props;
+
+  return (
+    <i
+      className={getClassName(type)}
+      style={{
+        ...pinStyle,
+        transform: `translate(${-size / 2}px,${-size}px)`
+      }}
+      onClick={onClick}
+    />
+  );
+}
