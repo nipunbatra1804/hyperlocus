985921f44c216e52e9a7816ec0d08e02a5d74c16
Aditya Swami <aswami@hatcher.com>
2019-04-02 11:13:29 +0800

    Revert "remove deploy"
    This reverts commit 98f96148afd2e62660e7cbdf0ab874444afa157e.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,5 +20,16 @@ module.exports = {
     env_production : {
       "NODE_ENV": "production"
     }
-  }]
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-52-74-35-245.ap-southeast-1.compute.amazonaws.com',
+      key: '/Users/adityaswami/Desktop/cs5224project.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:adityaswami93/hyperlocus.git',
+      path: '/var/www/hyperlocus',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
 }
