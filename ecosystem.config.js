ee43ba2afe6b2cb6c985f28d2789506acdb3e243
nipunbatra1804 <nipunbatra1994@gmail.com>
2019-04-17 20:16:43 +0800

    update deploy scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,8 @@ module.exports = {
       script: "npm",
       args: "run build",
       watch: false,
+      max_restarts: 0,
+      autorestart : false,
       env: {
         NODE_ENV: "development"
       },
@@ -38,7 +40,7 @@ module.exports = {
       repo: "git@github.com:adityaswami93/hyperlocus",
       path: "/home/ubuntu/hyperlocus",
       "post-deploy":
-        "git pull && npm install --prefix ./server && npm install --prefix ./client && pm2 startOrRestart ecosystem.config.js"
+        "git pull && npm install --prefix ./server && npm install --prefix ./client && pm2 start ecosystem.config.js"
     }
   }
 };
