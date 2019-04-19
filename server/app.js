5a4faeba28d71b29f874f7ad7e923b72257499f8
nipunbatra1804 <nipunbatra1994@gmail.com>
2019-04-19 11:03:39 +0800

    Add flag for BE server to server client/build

--- server/app.js
+++ server/app.js
@@ -4,9 +4,14 @@ const path = require("path");
 
 const app = express();
 app.use("/api/", index);
-app.use(express.static(path.join(__dirname + "/../client", "build")));
-+app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/../client", "build", "index.html"));
-});
+
+if (process.env.SERVE_FE) {
+  app.use(express.static(path.join(__dirname + "/../client", "build")));
+  +app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../client", "build", "index.html"));
+  });
+} else {
+  app.use("/", index);
+}
 
 module.exports = app;
