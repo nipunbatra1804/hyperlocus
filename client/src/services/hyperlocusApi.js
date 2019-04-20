de4a154391236c8c0c1ef0f5b5d81b0470c9248d
nipunbatra1804 <nipunbatra1994@gmail.com>
2019-04-20 15:03:44 +0800

    hardcode url for production

--- client/src/services/hyperlocusApi.js
+++ client/src/services/hyperlocusApi.js
@@ -8,7 +8,7 @@ const url = isValidHost(host)
   : "";
 console.log(url);
 const hyperlocusApi = axios.create({
-  baseURL: url,
+  baseURL: "http://www.hyperloc.us:4000/api",
   withCredentials: true
 });
 
