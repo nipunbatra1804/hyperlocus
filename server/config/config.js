844bcdd66a53a3ce1dfa39300d1b8cac5ec69878
nipunbatra1804 <nipunbatra1994@gmail.com>
2019-06-07 12:55:19 +0800

    add build scripts to root

--- server/config/config.js
+++ server/config/config.js
@@ -26,19 +26,9 @@ module.exports = {
     }
   },
   production: {
-    host: "hyperlocus.ctuxzjkhxcw0.ap-southeast-1.rds.amazonaws.com",
-    port: 5432,
-    username: "root",
-    password: "hunter1804",
-    database: "hyperlocus",
-    maxConcurrentQueries: 100,
-    dialectOptions: {
-      ssl: "Amazon RDS"
-    },
-    pool: { maxConnections: 5, maxIdleTime: 30 },
-    language: "en",
-    ssl: true,
-    dialect: "postgres",
-    options: {}
+    url: process.env.DATABASE_URL,
+    options: {
+      dialect: "postgres"
+    }
   }
 };
